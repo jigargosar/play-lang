f2b1584ab92dc643402a351e0b1f49e75667f8bc
Jigar Gosar <jigar.gosar@gmail.com>
2026-03-15 16:10:49 +0530

    Refactor parser: add remeda pipe, getOnly assert, explicit _lexer scope, .trim() grammar source

--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -3,36 +3,66 @@ import compile from 'nearley/lib/compile'
 import generate from 'nearley/lib/generate'
 import nearleyGrammar from 'nearley/lib/nearley-language-bootstrapped'
 import moo from 'moo'
+import { pipe } from 'remeda'
 
-const lexer = moo.compile({
-    NL: { match: /\n/, lineBreaks: true },
-    line: /[^\n]+/,
-})
+function getOnly<T>(arr: T[], errMsg: string): T {
+    if (arr.length !== 1) {
+        console.error(errMsg, JSON.stringify(arr, null, 2))
+        throw new Error(errMsg)
+    }
+    return arr[0]
+}
 
-const grammarSource = `
-@lexer lexer
+// The grammar directive `@lexer _lexer` tells Nearley to reference
+// a variable named `_lexer` at parse time. The generated JS code
+// from `generate()` contains `var grammar = { Lexer: _lexer, ... }`.
+// `evalGrammarJs` receives the lexer as a parameter named `_lexer`
+// so eval can resolve that reference.
 
-main -> line:* {% (d) => d[0].join('') %}
-line -> %line {% (d) => d[0].value %}
-      | %NL {% (d) => d[0].value %}
-`
-
-function compileGrammar(sourceCode: string) {
+function parseGrammarSource(grammarSource: string) {
     const grammarParser = new nearley.Parser(nearleyGrammar)
-    grammarParser.feed(sourceCode)
-    const grammarAst = grammarParser.results[0]
-    const grammarInfoObject = compile(grammarAst, {})
-    grammarInfoObject.lexer = lexer
-    const grammarJs = generate(grammarInfoObject, 'grammar')
+    grammarParser.feed(grammarSource)
+    return getOnly(grammarParser.results, 'Grammar parse: expected exactly 1 result')
+}
+
+function evalGrammarJs(grammarJs: string, _lexer: moo.Lexer) {
     const module = { exports: {} as Record<string, unknown> }
     eval(grammarJs)
     return module.exports
 }
 
-const compiledGrammar = compileGrammar(grammarSource)
+const playLexer = moo.compile({
+    NL: { match: /\n/, lineBreaks: true },
+    line: /[^\n]+/,
+})
+
+const grammarSource = `
+@lexer _lexer
+
+main -> line:* {% (d) => d[0].join('') %}
+line -> %line {% (d) => d[0].value %}
+      | %NL {% (d) => d[0].value %}
+`.trim()
+
+function compileGrammar(grammarSource: string, lexer: moo.Lexer) {
+    return pipe(
+        grammarSource,
+        parseGrammarSource,
+        (ast) => {
+            const info = compile(ast, {})
+            info.lexer = lexer
+            return info
+        },
+        (info) => generate(info, 'playGrammar'),
+        (js) => evalGrammarJs(js, lexer),
+        nearley.Grammar.fromCompiled,
+    )
+}
+
+const grammar = compileGrammar(grammarSource, playLexer)
 
 export function parse(source: string): string {
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(compiledGrammar))
+    const parser = new nearley.Parser(grammar)
     parser.feed(source)
-    return parser.results[0]
+    return getOnly(parser.results, 'Parse: expected exactly 1 result')
 }
